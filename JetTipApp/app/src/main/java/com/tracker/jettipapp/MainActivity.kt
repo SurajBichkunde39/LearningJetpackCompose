85b0a6b2f51a2df84b0eaaeb61032e92410957ab
surajbichkunde <surajbichkunde@gmail.com>
2022-12-22 15:29:47 +0530

    first commit Added initial JetTipApp

---
+++ JetTipApp/app/src/main/java/com/tracker/jettipapp/MainActivity.kt
@@ -0,0 +1,74 @@
+package com.tracker.jettipapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Shapes
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.tracker.jettipapp.ui.theme.JetTipAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyApp {
+                Text(text = "Hello Again!!")
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun TopHeader() {
+    Surface(modifier = Modifier
+        .fillMaxWidth()
+        .height(150.dp)
+        .clip(shape = CircleShape.copy(all = CornerSize(12.dp))),
+        color = Color(0xFFE9D7F7)
+    ) {
+        Column(
+            modifier = Modifier.padding(12.dp)
+
+        ) {
+            Text(text = "Total per person")
+            Text(text = "$138")
+        }
+    }
+}
+
+
+@Composable
+fun MyApp(content: @Composable () -> Unit){
+    JetTipAppTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colors.background
+        ) {
+            content()
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    JetTipAppTheme {
+        MyApp {
+            Text(text = "Hello Again!")
+        }
+    }
+}
